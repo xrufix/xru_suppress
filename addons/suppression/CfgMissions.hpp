1a47f43517e26c96b71848fb40ef7aae85d8f91b
rufix <stefan@ruhfass.com>
2018-09-10 19:55:08 +0200

    Fix wrong mission path

--- addons/suppression/CfgMissions.hpp
+++ addons/suppression/CfgMissions.hpp
@@ -1,7 +1,7 @@
 class CfgMissions {
     class Missions {
         class ADDON {
-            directory = QUOTE(x\MAINPREFIX\PREFIX\addons\COMPONENT\missions\ADDON.vr);
+            directory = QUOTE(MAINPREFIX\PREFIX\addons\COMPONENT\missions\ADDON.vr);
             author = "xrufix";
             displayName = COMPONENT_NAME;
             overviewText = "Try the suppression effects in 'xru Suppress'.";
